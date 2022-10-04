4901a8090e50c7733b2a3e67ee5c7c00e65314e2
Tomoko-hub <haisaitomoco@gmail.com>
2022-10-04 07:12:42 +0300

    add shoppingItems and create CartContainerComponent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import CartContainer from './components/CartContainer';
 import NavBar from './components/NavBar';
 
 function App() {
   return (
     <main>
       <NavBar />
+      <CartContainer />
     </main>
   );
 }
---
+++ src/components/CartContainer.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import CartItem from './CartItem';
+
+const CartContainer = () => {
+    const { amount, cartItems, total } = useSelector((state)=> state.cart);
+    if ( amount < 1 ) {
+        return (
+            <section>
+                <header className='cart'>
+                    <h2>Shopping Cart</h2>
+                    <h4 className='empty-cart'>Your bag is empty.</h4>
+                </header>
+            </section>
+        );
+    }
+
+    return (
+        <section className='cart'>
+            <header>
+                <h2>Your shipping bag.</h2>
+            </header>
+            <div>
+                {cartItems.map((item)=> {
+                    return <CartItem key={item.id} />
+                })}
+            </div>
+            <footer>
+                <hr />
+                <div className="cart-total">
+                    <h4>
+                        Total <span>{total} €</span>
+                    </h4>
+                </div>
+                <button className='btn clear-btn'>Delete All</button>
+            </footer>
+        </section>
+    )
+};
+
+export default CartContainer
